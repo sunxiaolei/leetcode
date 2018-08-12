4474448eb62025241034213c2d6f3f500d3b7364
sunxiaolei <xiaoleisun92@gmail.com>
2018-08-12 20:53:39 +0800

    p2

--- app/src/main/java/sun/xiaolei/leetcode/P2_add_two_numbers.java
+++ app/src/main/java/sun/xiaolei/leetcode/P2_add_two_numbers.java
@@ -73,6 +73,34 @@ public class P2_add_two_numbers {
         }
     }
 
+    /**
+     * 时间复杂度：O(max(m,n))，假设 m 和 n 分别表示 l1 和 l2 的长度，上面的算法最多重复 max(m,n) 次。
+     * 空间复杂度：O(max(m,n))， 新列表的长度最多为 max(m,n)+1。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbersO(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
     public static ListNode arr2Node(int[] arr) {
         ListNode node = null;
         for (int i = 0; i < arr.length; i++) {
